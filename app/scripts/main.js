cc24b9ca93d25fa71587bb71a7f6bcc30bc0866d
Thomas McCracken <thomas.alan.mccracken@gmail.com>
2014-10-08 15:11:28 -0400

    Accordion tabs added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('The Iron Yard Rocks');
+
+
+
+//Script from Refills to control the Tab-Accordion Container ========================
+$(document).ready(function () {
+  $('.accordion-tabs-minimal').each(function(index) {
+    $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+  });
+
+  $('.accordion-tabs-minimal').on('click', 'li > a', function(event) {
+    if (!$(this).hasClass('is-active')) {
+      event.preventDefault();
+      var accordionTabs = $(this).closest('.accordion-tabs-minimal')
+      accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+      $(this).next().toggleClass('is-open').toggle();
+      accordionTabs.find('.is-active').removeClass('is-active');
+      $(this).addClass('is-active');
+    } else {
+      event.preventDefault();
+    }
+  });
+});
+
+//End of Tab-Accordion Container Refill Script========================================
