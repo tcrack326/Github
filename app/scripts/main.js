dc71f15a39433be71c1ae19ebcfe03bfff656a08
Thomas McCracken <thomas.alan.mccracken@gmail.com>
2014-10-09 22:55:34 -0400

    Near completion of html with JSON data from Github

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,54 @@
 $(document).ready(function () {
 
+//Set up the Templates with Underscore
+
+var userUrl="https://api.github.com/users/tcrack326";
+var repoUrl="https://api.github.com/users/tcrack326/repos";
+var orgsUrl="https://api.github.com/users/tcrack326/orgs";
+
+var userNavTemplate = $('#userNav_template').html();
+var userNavRendered = _.template(userNavTemplate);
+
+var user_Template = $('#user_template').html();
+var userRendered = _.template(user_Template);
+
+var repo_Template = $('#repo_template').html();
+var repoRendered = _.template(repo_Template);
+
+var org_Template = $('#organization').html();
+var orgRendered = _.template(org_Template);
+
+//AJAX call to Github for User and Repo JSON data
+
+$.getJSON(userUrl).done(function (user_data) {
+
+  $('#userNav').append(userNavRendered(user_data));
+  $('.sidebar').append(userRendered(user_data));
+
+
+}).fail(function (){
+  alert("Failed to get user data from Github");
+});
+
+$.getJSON(repoUrl).done(function (repo_data) {
+  repo_data.forEach(function(repo){
+    $('.repoList').append(repoRendered(repo));
+  });
+
+
+}).fail(function (){
+  alert("Failed to get repo data from Github");
+});
+
+$.getJSON(orgsUrl).done(function (orgs_data){
+  orgs_data.forEach(function(org){
+    $('.organizations').append(orgRendered(org));
+  });
+
+
+}).fail(function (){
+  alert("Failed to get organizations data from Github");
+});
 
 //Script from Refills to control the Tab-Accordion Container ========================
 
